fdd544aa998c6f05d572dfa532f2d59a80c1228e
wangzk1900 <wangzk1900@gmail.com>
2021-01-10 22:31:24 +0800

    Extension 1: structopt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,25 +1,49 @@
-extern crate clap;
+extern crate structopt;
+
 use std::process::exit;
 
-use clap::{load_yaml, App};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "kvs", about = "key-value store client")]
+enum Opt {
+    /// Set the value of a string key to a string
+    Set {
+        #[structopt(name = "KEY", required = true, help = "a string key")]
+        key: String,
+        #[structopt(name = "VALUE", required = true, help = "a string value")]
+        value: String,
+    },
+    /// Get the string value of the a string key
+    Get {
+        #[structopt(name = "KEY", required = true, help = "a string key")]
+        key: String,
+    },
+    /// Remove a given key
+    Rm {
+        #[structopt(name = "VALUE", required = true, help = "a string key")]
+        key: String,
+    },
+}
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let opt = Opt::from_args();
 
-    match matches.subcommand() {
-        ("set", _sub_matches) => {
+    match opt {
+        Opt::Set {
+            key: _key,
+            value: _value,
+        } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        ("get", _sub_matches) => {
+        Opt::Get { key: _key } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        ("rm", _sub_matches) => {
+        Opt::Rm { key: _key } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        _ => unreachable!(),
     }
 }
